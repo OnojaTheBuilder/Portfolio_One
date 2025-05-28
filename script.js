c4e4acb15dabd870adf06ac71e7d585ae8791446
Israel Onoja <Israelonoja21@gmail.com>
2025-05-28 23:21:04 +0200

    Working on the responsiveness of my personal portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+/*const menuToggle = document.querySelector (".menu-toggle");
+const nav = document.querySelector (".nav");
+
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+}) */
+
+
+const menuToggle = document.querySelector(".hamburger-button");
+const nav = document.querySelector(".mobile-menu")
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+})
