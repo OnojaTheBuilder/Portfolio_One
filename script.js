6c276a1299b1cc86e6785a4d5d5f983df8faac0d
Israel Onoja <Israelonoja21@gmail.com>
2025-05-29 00:51:14 +0200

    Added navlinks functions code

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-/*const menuToggle = document.querySelector (".menu-toggle");
-const nav = document.querySelector (".nav");
-
-
-menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-}) */
-
+//hamburger open and close
 
 const menuToggle = document.querySelector(".hamburger-button");
 const nav = document.querySelector(".mobile-menu")
@@ -13,4 +6,22 @@ const nav = document.querySelector(".mobile-menu")
 menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("active");
     nav.classList.toggle("active");
+})
+
+//nav links function
+const navLinks = document.querySelectorAll("main-menu a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const sectionId = this.getAttribute("href");
+        const section = document.querySelector(sectionId);
+
+        section.scrollIntoView({
+            behavior: "smooth"
+        });
+        menuToggle.classList.remove("active");
+         nav.classList.remove("active");
+    })
 })
